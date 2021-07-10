d6cbd6a0d4ab9c7ad07b0b0401f630b486cdee83
springkyu <seika.k.r0610@gmail.com>
2021-07-10 13:54:35 +0000

    [UPDATE]2-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'todolist/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
+  post 'todolists' => 'todolists#create'
 end
